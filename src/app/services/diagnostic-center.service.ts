095fc72f1f1e08d25f8547c95925f18c46beb509
shivanshtalwar0 <shivanshtalwar0@gmail.com>
2020-05-06 23:05:39 +0530

    contact address added to diagnostic center

--- src/app/services/diagnostic-center.service.ts
+++ src/app/services/diagnostic-center.service.ts
@@ -1,37 +1,53 @@
 import {Injectable} from '@angular/core';
 import {DiagnosticCenter} from '../interfaces/diagnostic-center';
+import {HttpClient} from '@angular/common/http';
+import {map, mapTo, pluck, toArray} from 'rxjs/operators';
+import {of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DiagnosticCenterService {
-  centers: Array<DiagnosticCenter> = [];
-  updateCenter(centerId:number,centerName:string){
-   for(let i=0;i<this.centers.length;i++){
-     if(this.centers[i].centerId==centerId){
-       this.centers[i].centerName=centerName
-       this.centers[i].shouldUpdate=false
 
-     }
-   }
-
+  constructor(private http: HttpClient) {
   }
 
-  addCenter(centerName: string) {
-    this.centers.push({
-      centerName: centerName,
-      centerId: Math.abs(Math.random() * 10000),
-      shouldUpdate:false
-    });
+
+  centers: Array<DiagnosticCenter> = [];
+
+  updateCenter(centerId: number, center:DiagnosticCenter) {
+    return this.http.put('http://localhost:8080/update/'+centerId, center, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+
+  getCenter() {
+    this.http.get('http://localhost:8080/list').subscribe((value:any) => {
+      if(value.success){
+       this.centers=value.data.map((val)=>({centerId:val.id,centerName:val.centerName,
+         address:val.address,
+         contactNo:val.contactNo,
+         shouldUpdate:false}))
+      }
+  });
+  }
+
+  addCenter(data) {
+    return this.http.post('http://localhost:8080/new', data, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
   }
 
   deleteCenter(centerId: number) {
-    let centerToBeDeleted = this.centers.find(((value, index) => {
-      return value.centerId === centerId;
-    }));
-    this.centers.splice(this.centers.indexOf(centerToBeDeleted), 1);
+    return this.http.delete('http://localhost:8080/delete/'+centerId, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
   }
 
-  constructor() {
-  }
 }
