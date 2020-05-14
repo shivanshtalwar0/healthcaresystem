cc231059ccf83e30c8c17db3c609ea554f920532
Saurabh <kumar.sau0@gmail.com>
2020-05-14 22:22:27 +0530

    local commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,7 @@ import {DeletetestComponent} from './pages/testmanagement/deletetest/deletetest.
 import {ViewtestComponent} from './pages/testmanagement/viewtest/viewtest.component';
 import {UpdatetestComponent} from './pages/testmanagement/viewtest/updatetest/updatetest.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UserDetailComponent } from './pages/user-detail/user-detail.component';
 // import { UserHomeComponent } from './user-home/user-home.component';
 // import { CancelAppointmentComponent } from './cancel-appointment/cancel-appointment.component';
 
@@ -66,7 +67,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     LoginComponent,
     RegisterComponent,
     ViewtestComponent,
-    UpdatetestComponent
+    UpdatetestComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
